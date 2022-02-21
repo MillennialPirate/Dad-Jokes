c999825a4715ffe2087522cf13db55496461f3b9
DiligentCoder-20022001 <siddharthschandran45@gmail.com>
2022-02-21 18:17:08 +0530

    Registration and Logoff - Complete Authentication

--- DadJokes/Controllers/AccountController.cs
+++ DadJokes/Controllers/AccountController.cs
@@ -1,22 +1,85 @@
 ﻿using DadJokes.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Identity;
+using DadJokes.Models.ViewModels;
 namespace DadJokes.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ApplicationDbContext _context; 
-        public AccountController(ApplicationDbContext _db)
+        private readonly ApplicationDbContext _context;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+        
+        public AccountController(ApplicationDbContext _db, UserManager<ApplicationUser> _um, SignInManager<ApplicationUser> _sm, RoleManager<IdentityRole> _rm)
         {
             _context = _db;
+            _userManager = _um;
+            _signInManager = _sm;
+            _roleManager = _rm;
         }
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult Register()
+        public async Task<IActionResult> Register()
         {
+            //at the time of registering itself we will create the roles 
+            if(!_roleManager.RoleExistsAsync(Helper.Helper.Admin).GetAwaiter().GetResult())
+            {
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Helper.User));
+            }
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterVM model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email
+                };
+                //create the user 
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if(result.Succeeded)
+                {
+                    await _userManager.CreateAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if(ModelState.IsValid)
+            {
+                Console.WriteLine(model.Email);
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {   
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
         }
     }
 }
--- DadJokes/Program.cs
+++ DadJokes/Program.cs
@@ -23,6 +23,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
